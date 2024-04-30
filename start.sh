8ba6c5ad396fc694b425467c5890de2a40a0b29e
yagami891 <168579400+yagami891@users.noreply.github.com>
2024-04-30 23:10:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vijay14958/url-Shortner- /url-Shortner-
+  git clone https://github.com/yagami891/video-file/video-file
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /url-Shortner-
+  git clone $UPSTREAM_REPO /video-file
 fi
-cd /url-Shortner-
+cd /video-file
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
